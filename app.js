f62edfe2ac4334a66493dd5c8dc74e401b9586b4
unknowne1 <95596890+unknowne1@users.noreply.github.com>
2023-04-15 18:29:25 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,47 +1,31 @@
-const web3 = require('@solana/web3.js');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const solanaWeb3 = require('@solana/web3.js');
+const connection = new solanaWeb3.Connection(solanaWeb3.clusterApiUrl('devnet'));
 
-const connection = new web3.Connection(
-  web3.clusterApiUrl('devnet'),
-  'confirmed',
-);
-async function connectToWallet() {
-  if (window.solana) {
-    try {
-      // Connect to the wallet
-      const provider = window.solana;
-      await provider.connect();
+app.use(express.static('public'));
 
-      // Get the public key of the connected wallet
-      const publicKey = provider.publicKey;
-      console.log('Connected to wallet:', publicKey.toBase58());
-
-      // Check NFT ownership
-      const nftOwner = await checkNFTOwnership(publicKey);
-      if (nftOwner) {
-        // Grant access to the members-only area
-        grantAccess();
-      } else {
-        // Display an error message
-        displayErrorMessage('You do not own the required NFT to access this area.');
-      }
-    } catch (error) {
-      // Display an error message
-      displayErrorMessage('Failed to connect to wallet.');
-      console.error(error);
-    }
-  } else {
-    // Display an error message
-    displayErrorMessage('Solana wallet not found.');
+app.get('/api/check-nft-ownership/:nftPublicKey', async (req, res) => {
+  const nftPublicKey = req.params.nftPublicKey;
+  const nftAccountInfo = await connection.getAccountInfo(new solanaWeb3.PublicKey(nftPublicKey));
+  if (!nftAccountInfo) {
+    return res.json({ error: 'NFT account not found' });
   }
-}
-async function checkNFTOwnership(walletPublicKey) {
-  const nftMintAuthority = new web3.PublicKey('AhCWXkxEiNPD2WQ7eE9pDUku6zvgZpaU5jBpAzeXpHj4');
-  const tokenAccounts = await connection.getTokenAccountsByOwner(
-    walletPublicKey,
-    {mint: nftMintAuthority},
-  );
-  return tokenAccounts.value.length > 0;
-}
-function grantAccess() {
-  // TODO: Implement access control for the members-only area
-}
+
+  const ownerAddress = new solanaWeb3.PublicKey(nftAccountInfo.data.slice(64, 96));
+  const ownerAccountInfo = await connection.getAccountInfo(ownerAddress);
+  if (!ownerAccountInfo) {
+    return res.json({ error: 'Owner account not found' });
+  }
+
+  const ownerPublicKeyString = ownerAddress.toBase58();
+  const mintAuthorityPublicKeyString = new solanaWeb3.PublicKey(nftAccountInfo.data.slice(0, 32)).toBase58();
+  const membersOnlyAreaAccess = (mintAuthorityPublicKeyString === 'AhCWXkxEiNPD2WQ7eE9pDUku6zvgZpaU5jBpAzeXpHj4' && ownerPublicKeyString === req.query.walletPublicKey);
+
+  res.json({ isOwner: ownerPublicKeyString === req.query.walletPublicKey, membersOnlyAreaAccess });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
